812b4d935022ece417c21d3599bbb2f3701999ad
Katrinty <99185666+Ayemafx@users.noreply.github.com>
2023-11-12 04:31:07 +0500

    Add files via upload

---
+++ app/page.jsx
@@ -0,0 +1,41 @@
+
+
+const Home = () => (
+  <section className='w-full flex-center flex-col margin'>
+    <h1 className='head_text text-center'>
+      Discover Real Estate
+      <br className='max-md:hidden' />
+      <span className='blue_gradient text-center'> Smart Contracts</span>
+    </h1>
+    <p className='desc text-center margin'>
+    SmartCrow is an easy-to-use smart contract tool for real estate agents to create a digital escrow for real estate transactions utilizing Algorand blockchain technology.  Create escrow smart contracts for real estate anywhere, anytime, with SmartCrow!
+    </p>
+
+    <div className='sm:flex hidden btn-below margin'>
+          <>
+          <a href="https://www.smartcrow.info/">
+                <button
+                  type='button'
+                  className='black_btn about'
+                >
+                  About
+                </button>
+              </a>
+          </>
+          <div className="mx-2"></div>
+          <>
+          <a href="">
+                <button
+                  type='button'
+                  className='black_btn contract'
+                >
+                  Create Contract
+                </button>
+                </a>
+          </>
+      </div>
+
+  </section>
+);
+
+export default Home;
---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config
+  }
+}
+
+module.exports = nextConfig
